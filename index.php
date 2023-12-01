7f38e5ab94390af0afc7056ac9dc02bd31ad7998
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2023-12-01 11:11:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,24 @@ $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
 ?>
-<!doctype html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>FLY WIN</title><script defer="defer" src="/js/chunk-vendors.80e769af.js"></script><script defer="defer" src="/js/app.3a67099c.js"></script>
-<script>
-// document.addEventListener("visibilitychange", function() {
-//   if (document.hidden){
-//       console.log("Browser tab is hidden")
-//   } else {
-//       console.log("Browser tab is visible")
-//       location.reload();
-//   }
-// });
-</script>
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
 
+if ( ! isset( $wp_did_header ) ) {
 
+	$wp_did_header = true;
 
-<link href="/css/app.6bc18ed2.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but Fly win doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
