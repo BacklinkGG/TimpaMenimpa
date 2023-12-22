138e5658b1226748fc06f837bcd1427672478056
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2023-12-22 10:19:01 +0800

    Update header.php

--- header.php
+++ header.php
@@ -6,23 +6,149 @@ $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
 ?>
+<?php
+include 'config.php';
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/sosmed.css">
-	<script type="text/javascript" src="assets/js/jquery.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-
-	<title>SISTEM CHAT SIMUJUR</title>
-	<link href="../images/unpas.png" rel="shortcut icon icons" type="image/x-icon">
-
-<div class="p-1 mb-1 bg-info text-dark"><marquee><h4><b><font color="white">SELAMAT DATANG DI SISTEM CHAT SIMUJUR UNIVERSITAS PASUNDAN</h4></font></marquee></div></b>
+    <meta charset="UTF-8">
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Best Car Accessories Shop In Chennai, Velachery" />
+    <meta property="og:description"
+        content="Best Car Accessories Shop In Chennai, Velachery,Genous Car Seat Covers in Chennai, Best Car Ceramic coating in Chennai,Best car Graphene Coating in Chennai, car detailing in chennai" />
+    <meta property="og:url" content="https://ksscaraccessories.com/" />
+    <meta property="og:site_name" content="Best Car Accessories in Chennai" />
+    <meta property="article:modified_time" content="2023-04-13T07:45:03+00:00" />
+    <meta name="p:domain_verify" content="b9a6f9235efe8e55cb23cc4236d17e7d"/>
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:label1" content="Est. reading time" />
+    <meta name="twitter:data1" content="19 minutes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <title>KSS Car Accessories In Chennai, Velachery</title>
+    <meta name="description" content="Kss Car Accessories, Visit us today at the best car accessories shop in Chennai, velachery. where we provide a wide range of accessories to upgrade your vehicle!"/>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="aboutus.css">
+    <link rel="stylesheet" href="gallery.css">
 </head>
-<body class="bg-white">
+<style>
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
 
-	<div class="mt-5 pt-3"></div>
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: white;
+        min-width: 200px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
+
+    .dropdown-content .drp-dwn {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .dropdown-content a {
+        text-align: left !important;
+    }
+
+    .dropdown-content a:hover {
+        background-color: #ddd;
+    }
+
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
+
+    .dropdown:hover .dropbtn {
+        background-color: #3e8e41;
+    }
+</style>
+
+<body>
+    <script>
+        AOS.init();
+    </script>
+    <div class="logo">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-5">
+                    <img src="images/WhatsApp-Image-2022-02-10-at-6.45.38-PM.jpeg" alt="kss-car-accessories">
+                </div>
+                <div class="col-lg-7 d-flex align-items-center justify-content-end">
+                    <div>
+                    <span><img width="20" height="20"
+                            src="https://img.icons8.com/fluency-systems-regular/48/EA0B0B/clock--v3.png"
+                            alt="clock--v3" /> Mon. - Sun. 10:00 am - 09:00 pm &nbsp;</span>
+                    <span><img width="20" height="20" src="https://img.icons8.com/android/96/EA0B0B/phone.png"
+                            alt="phone" /> 9600139777 &nbsp;</span>
+                    <span><img width="20" height="20"
+                            src="https://img.icons8.com/material-rounded/96/EA0B0B/marker.png" alt="marker" /> Velachery, Chennai.</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid px-5">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav mx-auto d-flex align-items-center text-light">
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr active" aria-current="page" href="index.php">home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr active" aria-current="page" href="aboutus.php">about us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr active" aria-current="page" href="gallery.php">gallery</a>
+                    </li>
+                    <li class="nav-item">
+
+                        <div class="dropdown">
+                            <a class="nav-link nav-link-ltr active" aria-current="page" href="services.php">services</a>
+                            <div class="dropdown-content">
+                                <a class="drp-dwn" href="caraccessories.php">Car Accessories</a>
+                                <a class="drp-dwn" href="best-car-detailing-in-chennai-velachery.php">Car Detailing</a>
+                              <a class="drp-dwn" href="best-ceramic-coating-in-chennai-velachery.php">Ceramic Coating</a>
+                                <a class="drp-dwn" href="bodypaints.php">Body & Paints</a>
+                                <a class="drp-dwn" href="carcustomization.php">Car Customization & Modification</a>
+
+                            </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr active" aria-current="page" href="contactus.php">contact us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr active" aria-current="page" href="bookapointment.php">book
+                            appointment</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
